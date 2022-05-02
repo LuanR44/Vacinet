316023aa31b31409c7254d4de63d101e917635f5
Luan <78827184+LuanR44@users.noreply.github.com>
2022-05-02 11:15:23 -0300

    Create Atvity Vacina Gestantes Triplise

--- app/src/main/java/br/com/unisagrado/vacinet/telaVacinaGestantes.java
+++ app/src/main/java/br/com/unisagrado/vacinet/telaVacinaGestantes.java
@@ -2,13 +2,28 @@ package br.com.unisagrado.vacinet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class telaVacinaGestantes extends AppCompatActivity {
+
+    Button botaoVacinaT, botaoVacinaH, botaoVacinaI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_vacina_gestantes);
+
+        botaoVacinaT = findViewById(R.id.bntGestantesTriplice);
+
+        botaoVacinaT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent vacT = new Intent(getApplicationContext(), telaVacinaGestantesT.class);
+                startActivity(vacT);
+            }
+        });
     }
 }
---
+++ app/src/main/java/br/com/unisagrado/vacinet/telaVacinaGestantesT.java
@@ -0,0 +1,17 @@
+package br.com.unisagrado.vacinet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class telaVacinaGestantesT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_vacina_gestantes_t);
+    }
+}
