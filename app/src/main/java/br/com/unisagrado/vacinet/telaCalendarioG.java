a8dddb34e09591f12c5deacf2befbddff84b23a6
Luan <78827184+LuanR44@users.noreply.github.com>
2022-05-27 11:58:39 -0300

    Pequenas Mudanças na Activity Sobre o APP

--- app/src/main/java/br/com/unisagrado/vacinet/telaCalendarioG.java
+++ app/src/main/java/br/com/unisagrado/vacinet/telaCalendarioG.java
@@ -1,5 +1,6 @@
 package br.com.unisagrado.vacinet;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -16,5 +17,12 @@ public class telaCalendarioG extends AppCompatActivity {
 
         PDFView pdfView = findViewById(R.id.pdfView);
         pdfView.fromAsset("calend-sbim-gestante.pdf").show();
+
+        AlertDialog.Builder ok = new AlertDialog.Builder(telaCalendarioG.this);
+        ok.setTitle("Sugestão");
+        ok.setMessage("Caso esteja difícil de visualizar por conta do tamanho, utilize o zoom do seu aparelho!");
+        ok.setCancelable(false);
+        ok.setNegativeButton("ok",null);
+        ok.create().show();
     }
 }
